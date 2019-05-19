64114e9a49417464a295b520c7b80f33b8dfe64e
Brady Borman <bradenborman@hotmail.com>
2019-05-18 19:02:31 -0500

    Demo for passing vars in url plus passing them back to front end

--- src/main/java/com/Borman/cbbbluechips/controllers/ViewController.java
+++ src/main/java/com/Borman/cbbbluechips/controllers/ViewController.java
@@ -1,6 +1,8 @@
 package com.Borman.cbbbluechips.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +11,13 @@ public class ViewController {
     @RequestMapping("/")
     public String welcome() {
         return "index";
+    }
+
+    @RequestMapping("/2/{team}")
+    public String welcome2(@PathVariable String team, Model model) {
+        model.addAttribute("title", team);
+        System.out.println(team);
+        return "index2";
     }
 
 }
