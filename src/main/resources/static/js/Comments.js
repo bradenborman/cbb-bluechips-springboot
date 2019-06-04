7ac6ceb6461a78287c69495e4902cc5a47e963f1
Braden Borman <BBorman@shelterinsurance.com>
2019-06-04 10:32:26 -0500

    jquery methos that gets comment id preps for post to delete

---
+++ src/main/resources/static/js/Comments.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".icon-remove").click(function(){
+       var myClass = $(this).attr("id");
+       alert(myClass);
+
+       //TODO remove 'par' and sub and then post the correct one
+  });
+});
