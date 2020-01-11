1ff57dc4d26407f01770dc49e45a80cb1986518c
Brady Borman <bradenborman@hotmail.com>
2020-01-11 12:34:40 -0600

    Added things

--- src/main/java/com/Borman/cbbbluechips/controllers/RulesController.java
+++ src/main/java/com/Borman/cbbbluechips/controllers/RulesController.java
@@ -1,17 +1,30 @@
 package com.Borman.cbbbluechips.controllers;
 
+import com.Borman.cbbbluechips.services.CookieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 @RequestMapping("/rules")
 public class RulesController {
 
+    @Autowired
+    CookieService cookieService;
+
     @RequestMapping("")
-    public String portfolio(Model model) {
-     return "rules";
+    public String portfolio(Model model, HttpServletRequest request) {
+        model.addAttribute("loggedIn", cookieService.isLoggedIn(request));
+        return "rules";
+    }
+
+    @RequestMapping("/calculator")
+    public String calculator() {
+        return "calculator";
     }
 
 }
