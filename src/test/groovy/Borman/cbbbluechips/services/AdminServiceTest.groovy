3eab43d9b8ca945e9d16b49ad04d558e668c95c9
Brady Borman <bradenborman@hotmail.com>
2020-11-30 22:40:28 -0600

    Fixed test

--- src/test/groovy/Borman/cbbbluechips/services/AdminServiceTest.groovy
+++ src/test/groovy/Borman/cbbbluechips/services/AdminServiceTest.groovy
@@ -1,6 +1,7 @@
 package Borman.cbbbluechips.services
 
-
+import Borman.cbbbluechips.config.SportsDataApiConfig
+import Borman.cbbbluechips.models.UpdatePointSpreadRequest
 import spock.lang.Specification
 
 
@@ -22,12 +23,13 @@ class AdminServiceTest extends Specification {
     def 'validateChangeOfPointSpread VALID'() {
 
         given:
-            AdminService adminService = new AdminService(null, null, null, null, null)
-            List<Borman.cbbbluechips.models.UpdatePointSpreadRequest> updates = Arrays.asList(
-                    new Borman.cbbbluechips.models.UpdatePointSpreadRequest(nextPointSpread: -3.5),
-                    new Borman.cbbbluechips.models.UpdatePointSpreadRequest(nextPointSpread: 3.5),
-                    new Borman.cbbbluechips.models.UpdatePointSpreadRequest(nextPointSpread: -8.1),
-                    new Borman.cbbbluechips.models.UpdatePointSpreadRequest(nextPointSpread: 8.1)
+        SportsDataApiConfig sportsDataApiConfig = new SportsDataApiConfig(url: "", apiKey: "")
+            AdminService adminService = new AdminService(null, null, null, null, sportsDataApiConfig)
+            List<UpdatePointSpreadRequest> updates = Arrays.asList(
+                    new UpdatePointSpreadRequest(nextPointSpread: -3.5),
+                    new UpdatePointSpreadRequest(nextPointSpread: 3.5),
+                    new UpdatePointSpreadRequest(nextPointSpread: -8.1),
+                    new UpdatePointSpreadRequest(nextPointSpread: 8.1)
             )
         when:
             adminService.validateChangeOfPointSpread(updates)
