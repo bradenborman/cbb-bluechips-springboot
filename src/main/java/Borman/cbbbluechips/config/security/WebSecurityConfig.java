257fd22a2463940d78700272effdfb2ae0a9b858
Brady Borman <bradenborman@hotmail.com>
2020-12-12 12:56:50 -0600

    Added terms and conditions page

--- src/main/java/Borman/cbbbluechips/config/security/WebSecurityConfig.java
+++ src/main/java/Borman/cbbbluechips/config/security/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         http.authorizeRequests()
-                .antMatchers("/", "/rules", "/rules/**", "/user/logout")
+                .antMatchers("/", "/rules", "/rules/**", "/terms-and-services", "/user/logout")
                 .permitAll();
 
         http.authorizeRequests()
