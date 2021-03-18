2feea4f56bf12adc5d3ba0003b7616591e0cba40
Brady Borman <bradenborman@hotmail.com>
2021-03-18 18:54:03 -0500

    Added imgs -- put spread on portfilio page

--- src/main/java/Borman/cbbbluechips/models/enums/Ads.java
+++ src/main/java/Borman/cbbbluechips/models/enums/Ads.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public enum Ads {
 
-    CANDY_FACTORY("Candy Factory", "/ads/thecandyfactory_logo1.png", "https://www.thecandyfactoryonline.com/"),
+//    CANDY_FACTORY("Candy Factory", "/ads/candyFactory.png", "https://www.thecandyfactoryonline.com/"),
+    CANDY_FACTORY_2("Candy Factory", "/ads/thecandyfactory_logo1.png", "https://www.thecandyfactoryonline.com/"),
     Georgetown_Dental("Georgetown Dental", "/ads/GeorgetownDental.png", "http://www.ident.ws/template.jsp?doc=gregorydstevens"),
+    SHILOH_BAR_AND_GRILL("Shiloh Bar & Grill", "/ads/shiloh.jpg", "https://www.shilohbar-grill.com/"),
     Campus_Bar_and_Grill("Campus Bar & Grill", "/ads/b12 transparent_.png", "http://campusbarandgrill.com/");
 
     private String name;
