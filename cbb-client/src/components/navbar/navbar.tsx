0171dcb8ea141f86e2bb39e3bdfe1329c995c198
Braden Borman <bborman@shelterinsurance.com>
2021-05-23 18:39:45 -0500

    nav static stize again

--- cbb-client/src/components/navbar/navbar.tsx
+++ cbb-client/src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import { useHistory, useLocation } from "react-router";
 import classNames from "classnames";
 
@@ -7,38 +7,13 @@ export const Navbar: React.FC<INavbarProps> = (props: INavbarProps) => {
   let history = useHistory();
   let location: any = useLocation();
 
-  const [sticky, setSticky] = useState<boolean>(false);
-
-  const headerRef = useRef(null);
-
-  const handleScroll = () => {
-    if (headerRef.current) {
-      setSticky(headerRef.current.getBoundingClientRect().top <= 0);
-    }
-  };
-
-  useEffect(() => {
-    //  alert(sticky)
-  }, [sticky]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", () => handleScroll);
-    };
-  }, []);
-
   const returnToHome = (e: any) => {
     if (location.pathname !== "/login") history.push("/portfolio");
   };
 
   return (
     <div id="header-wrapper">
-      <header
-        ref={headerRef}
-        className={classNames("header", { stuck: sticky })}
-      >
+      <header className={classNames("header")}>
         <h1 onClick={returnToHome}>CBB Bluechips</h1>
       </header>
     </div>
